af8eadf8f3b9897062ec6fea92191e416ab50531
Niklas Arnitz <narnitz@churchtools.de>
2024-03-28 17:52:26 +0100

    small update

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { getComtradaForumCinemasMovies } from "@waslaeuftin/helpers/comtrada/forum-cinemas/getComtradaForumCinemasMovies";
-
-export const GET = async () => {
-  const testData = await getComtradaForumCinemasMovies("forum_rastatt");
-
-  return new Response(JSON.stringify(testData));
-};
