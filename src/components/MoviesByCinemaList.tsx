162b9dfca4186d8fafef04e4b2e20481a1b4f99f
Niklas Arnitz <narnitz@churchtools.de>
2024-03-30 00:08:10 +0100

    minor changes

--- src/components/MoviesByCinemaList.tsx
+++ src/components/MoviesByCinemaList.tsx
@@ -1,37 +1,31 @@
-import { Cities } from "@waslaeuftin/helpers/cities";
 import { type api } from "@waslaeuftin/trpc/server";
-import { Cinemas } from "@waslaeuftin/types/Cinemas";
-import { type CinemaSlugs } from "@waslaeuftin/types/CinemaSlugsSchema";
 import moment from "moment-timezone";
 import Link from "next/link";
 
 export const MoviesByCinemaList = ({
-  moviesByCinema,
+  city,
   showFilterByToday,
   showRemoveFilter,
-  city,
 }: {
-  moviesByCinema: Record<
-    CinemaSlugs,
-    Awaited<ReturnType<typeof api.movies.getMovies>>
-  >;
+  city: Awaited<ReturnType<typeof api.cities.getCityMoviesAndShowingsBySlug>>;
   showFilterByToday?: boolean;
   showRemoveFilter?: boolean;
-  city: string;
 }) => {
-  const cityObject = Cities[city ?? ""];
+  if (!city) {
+    return null;
+  }
 
   return (
     <>
       <div className="flex flex-row items-center justify-between gap-x-2 px-4 pt-4">
         <h1 className="flex flex-1 text-2xl font-bold">
           Diese Filme laufen {showRemoveFilter ? "heute" : "nächster Zeit"} in{" "}
-          {cityObject?.name}
+          {city.name}
         </h1>
         {showFilterByToday && (
           <div>
             <Link
-              href={`/city/${city}/today`}
+              href={`/city/${city.slug}/today`}
               className="rounded-lg border px-4 py-2 underline shadow-sm"
             >
               Heute
@@ -41,7 +35,7 @@ export const MoviesByCinemaList = ({
         {showRemoveFilter && (
           <div>
             <Link
-              href={`/city/${city}`}
+              href={`/city/${city.slug}`}
               className="rounded-lg border px-4 py-2 underline shadow-sm"
             >
               X Heute
@@ -49,50 +43,40 @@ export const MoviesByCinemaList = ({
           </div>
         )}
       </div>
-      {Object.keys(moviesByCinema)
-        .sort((a, b) => a.localeCompare(b))
-        .map((cinemaSlug) => {
-          const moviesInCinema =
-            moviesByCinema[cinemaSlug as CinemaSlugs] ?? [];
-          const cinema = Cinemas[cinemaSlug as CinemaSlugs];
-
-          if (!cinema) {
-            return null;
-          }
-
-          return (
-            <div key={cinema.slug} className="p-4">
-              <h1 className="text-xl font-bold">{cinema.name}</h1>
-              <div className="flex flex-col gap-y-4">
-                {moviesInCinema.map((movie) => (
-                  <div key={movie.name}>
-                    <h2 className="text-lg font-semibold">{movie.name}</h2>
-                    <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
-                      {movie.showings.map((showing) => {
-                        return (
-                          <Link
-                            href={showing.bookingUrl ?? "#"}
-                            key={`${cinema.slug}-${showing.dateTime.toISOString()}-${movie.name}`}
-                            className="flex flex-col rounded-lg border border-gray-200 bg-white p-4 shadow-md hover:border-gray-300"
-                          >
-                            {moment(showing.dateTime).format(
-                              "DD.MM.YYYY - HH:mm",
-                            )}
-                            {showing.showingAdditionalData && (
-                              <div className="text-xs text-gray-500">
-                                {showing.showingAdditionalData}
-                              </div>
-                            )}
-                          </Link>
-                        );
-                      })}
-                    </div>
+      {city.cinemas.map((cinema) => {
+        return (
+          <div key={cinema.slug} className="p-4">
+            <h1 className="text-xl font-bold">{cinema.name}</h1>
+            <div className="flex flex-col gap-y-4">
+              {cinema.movies.map((movie) => (
+                <div key={movie.name}>
+                  <h2 className="text-lg font-semibold">{movie.name}</h2>
+                  <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
+                    {movie.showings.map((showing) => {
+                      return (
+                        <Link
+                          href={showing.bookingUrl ?? "#"}
+                          key={`${cinema.slug}-${showing.dateTime.toISOString()}-${movie.name}`}
+                          className="flex flex-col rounded-lg border border-gray-200 bg-white p-4 shadow-md hover:border-gray-300"
+                        >
+                          {moment(showing.dateTime).format(
+                            "DD.MM.YYYY - HH:mm",
+                          )}
+                          {showing.showingAdditionalData && (
+                            <div className="text-xs text-gray-500">
+                              {showing.showingAdditionalData}
+                            </div>
+                          )}
+                        </Link>
+                      );
+                    })}
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-          );
-        })}
+          </div>
+        );
+      })}
     </>
   );
 };
--- src/types/guards/isMovie.ts
+++ src/types/guards/isMovie.ts
@@ -1,4 +1,9 @@
-import { type Movie, MovieSchema } from "../Movie";
+import { type Movie } from "@prisma/client";
+import { z } from "zod";
+
+const MovieSchema = z.object({
+  name: z.string(),
+});
 
 export const isMovie = (input: unknown): input is Movie => {
   return MovieSchema.safeParse(input).success;
