859454e2b08c1b014e5469ce322336e33307d79f
Niklas Arnitz <narnitz@churchtools.de>
2024-03-31 09:47:15 +0200

    added signout button

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { Button } from "@waslaeuftin/components/ui/button";
+import { signOut } from "next-auth/react";
+
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div>
+      Admin
+      <div>
+        <Button onClick={() => signOut()}>Logout</Button>
+      </div>
+    </div>
+  );
 }
