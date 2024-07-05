d15e4e3f23f202de273ef68647472a80701ed69d
Niklas Arnitz <narnitz@churchtools.de>
2024-07-05 17:38:55 +0200

    added create cineplex script

---
+++ scripts/create-cineplex-cinemas.ts
@@ -0,0 +1,79 @@
+import { ArrayHelper } from "@ainias42/js-helper";
+import { ApolloClient, InMemoryCache, HttpLink, gql } from "@apollo/client";
+import { db } from "@waslaeuftin/server/db";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://graphql-api.app.cineplex.de",
+  }),
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    cinemas {
+      id
+      name
+      city
+    }
+  }
+`;
+
+const { data } = await client.query<{
+  cinemas: {
+    id: string;
+    name: string;
+    city: string;
+  }[];
+}>({
+  query,
+});
+
+await ArrayHelper.asyncForEach(data.cinemas, async (cinema) => {
+  console.log(cinema.city);
+
+  let city = await db.city.findFirst({
+    where: {
+      OR: [
+        {
+          name: cinema.city,
+        },
+        {
+          slug: cinema.city.toLowerCase().replace(/\s/g, "_"),
+        },
+      ],
+    },
+  });
+
+  if (!city) {
+    city = await db.city.create({
+      data: {
+        name: cinema.city,
+        slug: cinema.city.toLowerCase().replace(/\s/g, "_"),
+      },
+    });
+  }
+
+  const dbCinema = await db.cinema.findFirst({
+    where: {
+      slug: cinema.name.toLowerCase().replace(/\s/g, "_"),
+    },
+  });
+
+  if (!dbCinema) {
+    await db.cinema.create({
+      data: {
+        name: cinema.name,
+        slug: cinema.name.toLowerCase().replace(/\s/g, "_"),
+        city: {
+          connect: {
+            id: city.id,
+          },
+        },
+        cineplexCinemaId: cinema.id,
+      },
+    });
+  }
+
+  console.log(`Cinema with name ${cinema.name} created`);
+});
