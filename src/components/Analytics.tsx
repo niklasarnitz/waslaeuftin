c1197155a2995900736ecd6df6142d2e098e1954
Niklas Arnitz <narnitz@churchtools.de>
2026-03-24 17:25:04 +0100

    fixed analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,23 +1,14 @@
 import { headers } from "next/headers";
 
 export const Analytics = async () => {
-  const hostname = (await headers()).get("host");
+    const hostname = (await headers()).get("host");
 
-  if (hostname === "whatsshowing.in") {
-    return (
-      <script
-        defer
-        src="https://umami.app.niklas.services/script.js"
-        data-website-id="837c78d5-b2ee-4c05-8075-9eab3b5d6980"
-      ></script>
-    );
-  } else if (hostname === "waslaeuft.in") {
-    return (
-      <script
-        defer
-        src="https://umami.app.niklas.services/script.js"
-        data-website-id="7538dcdd-2bff-4310-8b80-73c666f2d90a"
-      ></script>
-    );
-  }
+    if (hostname === "waslaeuft.in") {
+        return (
+            <script defer src="https://api.arnitz.org/script.js" data-website-id="29498997-c3d1-4b41-9129-ee2a0a25ede6">
+            </script>
+        );
+    }
+
+    return null;
 };
