275ca811ee9e9a2bbfc3170c43198a3f1362d238
niklasarnitz <24507162+niklasarnitz@users.noreply.github.com>
2026-06-08 12:36:00 +0000

    🔒 Fix XSS in JsonLd by using serialize-javascript
    Replaces direct use of dangerouslySetInnerHTML with manual regex replacements in safeJsonLd.ts with the use of serialize-javascript to securely serialize JsonLd data. Introduces a standardized JsonLd component and updates all call sites to use it.

--- src/helpers/safeJsonLd.ts
+++ src/helpers/safeJsonLd.ts
@@ -1,8 +1,5 @@
+import serialize from "serialize-javascript";
+
 export function safeJsonLd(data: unknown): string {
-  return JSON.stringify(data)
-    .replace(/</g, '\\u003c')
-    .replace(/>/g, '\\u003e')
-    .replace(/&/g, '\\u0026')
-    .replace(/\u2028/g, '\\u2028')
-    .replace(/\u2029/g, '\\u2029');
+  return serialize(data, { isJSON: true });
 }
